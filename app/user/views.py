06cdda69db2dfbeec5ee7d4ce79dd27407f442d0
yash jain <yashj133.yj@gmail.com>
2024-05-14 17:06:58 +0530

    Implement token API - Added API to create token - Added serializer for tokens - Updated urls and views - Added required tests

--- app/user/views.py
+++ app/user/views.py
@@ -2,9 +2,18 @@
 Views for the user API
 """
 from rest_framework import generics
-from user.serializer import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializer import (UserSerializer, AuthTokenSerializer)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
